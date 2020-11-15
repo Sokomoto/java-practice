46e23890a9ae8bda37ab25252393188d9d6758fe
Sokomoto <wdws2342319@gmail.com>
2020-11-15 18:55:18 +0900

    java-practice2

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-var hello = 'Hello World';
-alert(hello);
+// メイン部分
+var alertString;
+alertString = addString("WebCamp");
+
+// 作成した関数を呼び出す
+alert(alertString);
+
+// 作成した関数
+function addString(strA){
+	var addstr = "Hello " + strA;
+	return addstr;
+}
